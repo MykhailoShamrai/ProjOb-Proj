81a467ea1eaef6b5d74278f43bcd4c1a5a8dfacd
Mykhailo Shamrai <mike.shamrai@gmail.com>
2024-03-10 00:50:40 +0100

    Changes: visitor pattern for creating flight objects and adding database

--- ProjOb_project/Parsers/FtrParser.cs
+++ ProjOb_project/Parsers/FtrParser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjOb_project.Parsers
+﻿namespace ProjOb_project.Parsers
 {
     // Class FtrParser inherited from Parser class
     internal class FtrParser : Parser
